3663d4dcadce18cc76f8c28b23302c8f2d990b4b
adymytrov <adymytrov@star.global>
2022-06-05 23:49:03 +0300

    Add region info

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const TelegramApi = require('node-telegram-bot-api')
-const {gameOptions, againOptions} = require('./options')
-const sequelize = require('./db');
-const UserModel = require('./models');
-
+const {cityOptions, againOptions} = require('./options')
+const EVACUATION_INFO = require('./message')
+const cityList = require('./filter')
 const token = '5455523407:AAGwSNYgu5U7wncrr0nsJAUfrNiAo6NXfGQ'
-
 const bot = new TelegramApi(token, {polling: true})
-
 const chats = {}
 
-
-const startGame = async (chatId) => {
-    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`);
-    const randomNumber = Math.floor(Math.random() * 10)
-    chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+const chooseRegion = async (chatId) => {
+    await bot.sendMessage(chatId, 'Оберіть будь ласка Ваш регіон!', cityOptions);
 }
 
 const start = async () => {
 
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-    } catch (e) {
-        console.log('Подключение к бд сломалось', e)
-    }
-
-    
+    bot.setMyCommands([
+      {command: '/start', description: 'Обрати регіон.'},
+      {command: '/info', description: 'Інформація.'},
+      {command: '/region', description: 'Обрати регіон.'},
+      {command: '/evacuation', description: 'Евакуація.'},
+    ])
 
     bot.on('message', async msg => {
         const text = msg.text;
@@ -34,39 +25,36 @@ const start = async () => {
 
         try {
             if (text === '/start') {
-                await UserModel.create({chatId})
-                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-                return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот автора ютуб канала ULBI TV`);
+                return bot.sendMessage(chatId, `Доброго дня ${msg.from.first_name} У цьому боті Ви знайдете всю корисну інформацію щодо вашого регіону!`);
             }
             if (text === '/info') {
-                const user = await UserModel.findOne({chatId})
-                return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`);
+                return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name}`);
             }
-            if (text === '/game') {
-                return startGame(chatId);
+            if (text === '/region') {
+                return chooseRegion(chatId);
             }
-            return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
+            if (text === '/evacuation') {
+              return bot.sendMessage(chatId, EVACUATION_INFO,{parse_mode : "HTML"});
+          }
+            return bot.sendMessage(chatId, 'Я Вас не розумію, спробуйте ще!)');
         } catch (e) {
-            return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
+            return bot.sendMessage(chatId, 'Щось пішло не так!(');
         }
-
     })
 
     bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
+        let filteredInfo = cityList.filter(friend => friend.city === data).map(friend => friend.info) + '';
+
         if (data === '/again') {
-            return startGame(chatId)
-        }
-        const user = await UserModel.findOne({chatId})
-        if (data == chats[chatId]) {
-            user.right += 1;
-            await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions);
+          return chooseRegion(chatId)
+        } else if(data) {
+          await bot.sendMessage(chatId, filteredInfo, {parse_mode : "HTML"}, againOptions);
+          // await bot.sendMessage(chatId, `Обрати інший регіон`, againOptions);
         } else {
-            user.wrong += 1;
-            await bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions);
+          await bot.sendMessage(chatId, `Обрати інший регіон`, againOptions);
         }
-        await user.save();
     })
 }
 
--- options.js
+++ options.js
@@ -1,11 +1,34 @@
+const EVACUATION_INFO = require('./message')
+
 module.exports = {
-  gameOptions: {
+  cityOptions: {
      reply_markup: JSON.stringify({
          inline_keyboard: [
-             [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
-             [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
-             [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
-             [{text: '0', callback_data: '0'}],
+             [{text: 'АР Крим', callback_data: `1`}],
+             [{text: 'Вінницька', callback_data: '2'}],
+             [{text: 'Волинська', callback_data: '3'}],
+             [{text: 'Дніпропетровська', callback_data: '4'}],
+             [{text: 'Донецька', callback_data: '5'}],
+             [{text: 'Житомирська', callback_data: '6'}],
+             [{text: 'Закарпатська', callback_data: '7'}],
+             [{text: 'Запорізька', callback_data: '8'}],
+             [{text: 'Івано-Франківська', callback_data: '9'}],
+             [{text: 'Київська', callback_data: '10'}],
+             [{text: 'Кіровоградська', callback_data: '11'}],
+             [{text: 'Луганська', callback_data: '12'}],
+             [{text: 'Львівська', callback_data: '13'}],
+             [{text: 'Миколаївська', callback_data: '14'}],
+             [{text: 'Одеська', callback_data: '15'}],
+             [{text: 'Полтавська', callback_data: '16'}],
+             [{text: 'Рівненська', callback_data: '17'}],
+             [{text: 'Сумська', callback_data: '18'}],
+             [{text: 'Тернопільська', callback_data: '19'}],
+             [{text: 'Харківська', callback_data: '20'}],
+             [{text: 'Херсонська', callback_data: '21'}],
+             [{text: 'Хмельницька', callback_data: '22'}],
+             [{text: 'Черкаська', callback_data: '23'}],
+             [{text: 'Чернівецька', callback_data: '24'}],
+             [{text: 'Чернігівська', callback_data: '25'}],
          ]
      })
  },
@@ -13,7 +36,7 @@ module.exports = {
  againOptions: {
      reply_markup: JSON.stringify({
          inline_keyboard: [
-             [{text: 'Играть еще раз', callback_data: '/again'}],
+             [{text: 'Обрати інший регіон.', callback_data: '/again'}],
          ]
      })
  }
